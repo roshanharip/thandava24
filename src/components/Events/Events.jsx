53fd0977556b93e989c947bf4ef9a4b1a08e79c3
ashishshaiju <74810128+ashishshaiju@users.noreply.github.com>
2024-03-18 04:31:33 +0530

    Add events page

---
+++ src/components/Events/Events.jsx
@@ -0,0 +1,41 @@
+import "./Events.scss";
+
+const Events = () => {
+	return (
+		<div className="events">
+			<div className="events_heading">
+				<h3>Events</h3>
+			</div>
+			<div className="events_cards-container">
+				<div className="cards-container">
+					<div className="cards"><a href="">
+						<img src="https://loremflickr.com/1300/1300/paris" alt="img" /> </a>
+					</div>
+					<div className="cards"><a href="">
+						<img src="https://loremflickr.com/1300/1300/dubai" alt="img" /> </a>
+					</div>
+					<div className="cards"><a href="">
+						<img src="https://loremflickr.com/1300/1300/sports" alt="img" /> </a>
+					</div>
+					<div className="cards"><a href="">
+						<img src="https://loremflickr.com/1300/1300/audi" alt="img" /> </a>
+					</div>
+					<div className="cards"><a href="">
+						<img src="https://loremflickr.com/1300/1300/dog" alt="img" /> </a>
+					</div>
+					<div className="cards"><a href="">
+						<img src="https://loremflickr.com/1300/1300/car" alt="img" /> </a>
+					</div>
+					<div className="cards"><a href="">
+						<img src="https://loremflickr.com/1300/1300/hen" alt="img" /> </a>
+					</div>
+					<div className="cards"><a href="">
+						<img src="https://loremflickr.com/1300/1300/newyork" alt="img" /></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Events;
