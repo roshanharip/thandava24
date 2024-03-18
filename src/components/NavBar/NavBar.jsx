33894a324793245dc789396693bdf7fb0acfe159
roshanharip <roshanharip02@gmail.com>
2024-03-18 16:36:10 +0530

    Changes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './NavBar.scss';
 import Logo from '../../assets/logo.png';
 import { Outlet } from 'react-router';
+import { Link } from 'react-router-dom';
 const NavBar = () => {
   return (
     <>
@@ -11,8 +12,8 @@ const NavBar = () => {
                 <img src={Logo} alt="" />
             </div>
             <div className='Links'>
-                <a href="/">Home</a>
-                <a href="/events">Events</a>
+                <Link to='/'>Home</Link>
+                <Link  to='/events'>Events</Link>
             </div>
     </div>
     <Outlet/>
