deeec8e7550f9ed4b265d1987b2d6ede7ffc93b0
ashishshaiju <74810128+ashishshaiju@users.noreply.github.com>
2024-03-18 13:20:38 +0530

    Add Cards

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -8,29 +8,50 @@ const Events = () => {
 			</div>
 			<div className="events_cards-container">
 				<div className="cards-container">
-					<div className="cards"><a href="">
-						<img src="https://loremflickr.com/1300/1300/paris" alt="img" /> </a>
+					<div className="cards"><a href="#">
+						<img src="../../assets/Events/1.png" alt="img1" /> </a>
 					</div>
-					<div className="cards"><a href="">
-						<img src="https://loremflickr.com/1300/1300/dubai" alt="img" /> </a>
+					<div className="cards"><a href="#">
+						<img src="../../assets/Events/2.png" alt="img2" /> </a>
 					</div>
-					<div className="cards"><a href="">
-						<img src="https://loremflickr.com/1300/1300/sports" alt="img" /> </a>
+					<div className="cards"><a href="#">
+						<img src="../../assets/Events/3.png" alt="img3" /> </a>
 					</div>
-					<div className="cards"><a href="">
-						<img src="https://loremflickr.com/1300/1300/audi" alt="img" /> </a>
+					<div className="cards"><a href="#">
+						<img src="../../assets/Events/4.png" alt="img4" /> </a>
 					</div>
-					<div className="cards"><a href="">
-						<img src="https://loremflickr.com/1300/1300/dog" alt="img" /> </a>
+					<div className="cards"><a href="#">
+						<img src="../../assets/Events/5.png" alt="img5" /> </a>
 					</div>
-					<div className="cards"><a href="">
-						<img src="https://loremflickr.com/1300/1300/car" alt="img" /> </a>
+					<div className="cards"><a href="#">
+						<img src="../../assets/Events/6.png" alt="img6" /> </a>
 					</div>
-					<div className="cards"><a href="">
-						<img src="https://loremflickr.com/1300/1300/hen" alt="img" /> </a>
+					<div className="cards"><a href="#">
+						<img src="../../assets/Events/7.png" alt="img7" /> </a>
 					</div>
-					<div className="cards"><a href="">
-						<img src="https://loremflickr.com/1300/1300/newyork" alt="img" /></a>
+					<div className="cards"><a href="#">
+						<img src="../../assets/Events/8.png" alt="img8" /></a>
+					</div>
+					<div className="cards"><a href="#">
+						<img src="../../assets/Events/9.png" alt="img9" /></a>
+					</div>
+					<div className="cards"><a href="#">
+						<img src="../../assets/Events/10.png" alt="img10" /></a>
+					</div>
+					<div className="cards"><a href="#">
+						<img src="../../assets/Events/11.png" alt="img11" /></a>
+					</div>
+					<div className="cards"><a href="#">
+						<img src="../../assets/Events/12.png" alt="img12" /></a>
+					</div>
+					<div className="cards"><a href="#">
+						<img src="../../assets/Events/13.png" alt="img13" /></a>
+					</div>
+					<div className="cards"><a href="#">
+						<img src="../../assets/Events/14.png" alt="img14" /></a>
+					</div>
+					<div className="cards"><a href="#">
+						<img src="../../assets/Events/15.png" alt="img15" /></a>
 					</div>
 				</div>
 			</div>
