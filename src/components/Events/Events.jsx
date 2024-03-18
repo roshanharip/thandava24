a929d86a2aead8348cdada03d238f23799030bff
ashishshaiju <74810128+ashishshaiju@users.noreply.github.com>
2024-03-18 14:42:37 +0530

    Add Resp

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,4 +1,16 @@
 import "./Events.scss";
+import img1 from "../../assets/Events/1.png";
+import img2 from "../../assets/Events/2.png";
+import img3 from "../../assets/Events/3.png";
+import img4 from "../../assets/Events/4.png";
+import img5 from "../../assets/Events/5.png";
+import img6 from "../../assets/Events/6.png";
+import img10 from "../../assets/Events/10.png";
+import img11 from "../../assets/Events/11.png";
+import img12 from "../../assets/Events/12.png";
+import img13 from "../../assets/Events/13.png";
+import img14 from "../../assets/Events/14.png";
+import img15 from "../../assets/Events/15.png";
 
 const Events = () => {
 	return (
@@ -8,53 +20,69 @@ const Events = () => {
 			</div>
 			<div className="events_cards-container">
 				<div className="cards-container">
-					<div className="cards"><a href="#">
-						<img src="../../assets/Events/1.png" alt="img1" /> </a>
+					<div className="cards">
+						<a href="#">
+							<img src={img1} alt="img1" />{" "}
+						</a>
 					</div>
-					<div className="cards"><a href="#">
-						<img src="../../assets/Events/2.png" alt="img2" /> </a>
+					<div className="cards">
+						<a href="#">
+							<img src={img2} alt="img2" />{" "}
+						</a>
 					</div>
-					<div className="cards"><a href="#">
-						<img src="../../assets/Events/3.png" alt="img3" /> </a>
+					<div className="cards">
+						<a href="#">
+							<img src={img3} alt="img3" />{" "}
+						</a>
 					</div>
-					<div className="cards"><a href="#">
-						<img src="../../assets/Events/4.png" alt="img4" /> </a>
+					<div className="cards">
+						<a href="#">
+							<img src={img4} alt="img4" />{" "}
+						</a>
 					</div>
-					<div className="cards"><a href="#">
-						<img src="../../assets/Events/5.png" alt="img5" /> </a>
+					<div className="cards">
+						<a href="#">
+							<img src={img5} alt="img5" />{" "}
+						</a>
 					</div>
-					<div className="cards"><a href="#">
-						<img src="../../assets/Events/6.png" alt="img6" /> </a>
+					<div className="cards">
+						<a href="#">
+							<img src={img6} alt="img6" />{" "}
+						</a>
 					</div>
-					<div className="cards"><a href="#">
-						<img src="../../assets/Events/7.png" alt="img7" /> </a>
+					<div className="cards">
+						<a href="#">
+							<img src={img10} alt="img10" />
+						</a>
 					</div>
-					<div className="cards"><a href="#">
-						<img src="../../assets/Events/8.png" alt="img8" /></a>
+					<div className="cards">
+						<a href="#">
+							<img src={img11} alt="img11" />
+						</a>
 					</div>
-					<div className="cards"><a href="#">
-						<img src="../../assets/Events/9.png" alt="img9" /></a>
+					<div className="cards">
+						<a href="#">
+							<img src={img12} alt="img12" />
+						</a>
 					</div>
-					<div className="cards"><a href="#">
-						<img src="../../assets/Events/10.png" alt="img10" /></a>
+					<div className="cards">
+						<a href="#">
+							<img src={img13} alt="img13" />
+						</a>
 					</div>
-					<div className="cards"><a href="#">
-						<img src="../../assets/Events/11.png" alt="img11" /></a>
+					<div className="cards">
+						<a href="#">
+							<img src={img14} alt="img14" />
+						</a>
 					</div>
-					<div className="cards"><a href="#">
-						<img src="../../assets/Events/12.png" alt="img12" /></a>
-					</div>
-					<div className="cards"><a href="#">
-						<img src="../../assets/Events/13.png" alt="img13" /></a>
-					</div>
-					<div className="cards"><a href="#">
-						<img src="../../assets/Events/14.png" alt="img14" /></a>
-					</div>
-					<div className="cards"><a href="#">
-						<img src="../../assets/Events/15.png" alt="img15" /></a>
+					<div className="cards">
+						<a href="#">
+							<img src={img15} alt="img15" />
+						</a>
 					</div>
 				</div>
 			</div>
+			<img src="../../assets/Events/11.png" alt="" />
 		</div>
 	);
 };
